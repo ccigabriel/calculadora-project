f74454903de3e0c07386ae658628c16bf07390cb
ccigabriel <gabrielmurucci5@gmail.com>
2024-05-10 16:24:44 -0300

    Adicionando o README

---
+++ #Calculadora com interface que lê os gas.py
@@ -0,0 +1,19 @@
+# Calculadora com interface que lê os gastos do cliente, onde esses gastos podem ser definidos por ele. E que no final diga quanto dinheiro sobrou calculando isso pelo valor do salario dele
+gastos = {}
+
+while True:
+
+    valor_gastos = float(input('Digite o seu gasto: R$ '))
+    if valor_gastos == str:
+        print('Por favor, digite um valor. ')
+
+    nome_gastos = input('Qual o nome do seu gasto? ')
+    # Usar a variável nome_gastos como chave
+    gastos[nome_gastos] = valor_gastos
+
+    continuar = str(input('Quer continuar?  S/N  ')).upper().strip()
+    if continuar != 'S':
+        break
+
+for nome, valor in gastos.items():  # Items = items do dicionario 'gastos'
+    print(f'{nome}: R${valor:.2f}')
---
+++ readme.md
@@ -0,0 +1,16 @@
+## Calculadora 🧮
+
+Projeto de criação de uma calculadora que calcula e registra os gastos mensais do usuário, lê o salário e retorna o valor que sobrou. 
+
+
+
+
+## Autores
+
+- [@ccigabriel](https://github.com/ccigabriel)
+
+
+
+## Suporte
+
+Para suporte, mande um email para gabrielmurucci5@gmail.com
